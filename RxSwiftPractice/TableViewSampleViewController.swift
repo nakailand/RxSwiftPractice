386f9fb1c98c681b5af81a0046805bb18f3671bb
nakazy <s.nakajima0523@gmail.com>
2016-02-06 23:01:55 +0800

    add tableView sample

---
+++ RxSwiftPractice/TableViewSampleViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewSampleViewController.swift
+//  RxSwiftPractice
+//
+//  Created by nakazy on 2016/02/06.
+//  Copyright © 2016年 nakazy. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class TableViewSampleViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    let disposeBag = DisposeBag()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let items = Observable.just([
+            "First Item",
+            "Second Item",
+            "Third Item",
+        ])
+        
+        items
+            .bindTo(tableView.rx_itemsWithCellIdentifier("Cell", cellType: UITableViewCell.self)) { (row, element, cell) in
+                cell.textLabel?.text = element
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var textFieldSampleButton: UIButton!
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var tableViewSampleButton: UIButton!
     let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +29,13 @@ class ViewController: UIViewController {
             .subscribeNext {
                 let textFieldSampleViewController = UIStoryboard(name: "TextFieldSample", bundle: nil).instantiateInitialViewController()
                 self.navigationController?.pushViewController(textFieldSampleViewController!, animated: true)
+            }
+            .addDisposableTo(disposeBag)
+        
+        tableViewSampleButton.rx_tap
+            .subscribeNext {
+                let tableViewSampleViewController = UIStoryboard(name: "TableViewSample", bundle: nil).instantiateInitialViewController()
+                self.navigationController?.pushViewController(tableViewSampleViewController!, animated: true)
             }
             .addDisposableTo(disposeBag)
     }
